0894a1b1ca8af6a92f38e6e001383d49c7ac5b10
Manvir Singh <168892034+manvirsingh1716@users.noreply.github.com>
2025-07-29 00:39:28 +0530

    new website script

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,43 @@
 import Component from "../nifty-dashboard"
+import { Head } from "next/document"
 
 export default function Page() {
-  return <Component />
+  return (
+    <>
+      <Head>
+        <title>NiftyNiti - Predict the Next Move of NIFTY 50</title>
+
+        {/* WebSite Schema */}
+        <script type="application/ld+json" dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            "url": "https://niftyniti.in",
+            "name": "NiftyNiti",
+            "potentialAction": {
+              "@type": "SearchAction",
+              "target": "https://niftyniti.in/?s={search_term_string}",
+              "query-input": "required name=search_term_string"
+            }
+          })
+        }} />
+
+        {/* Organization Schema */}
+        <script type="application/ld+json" dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "name": "NiftyNiti",
+            "url": "https://niftyniti.in",
+            "logo": "https://niftyniti.in/logo.png", // replace with actual logo URL
+            "sameAs": [
+              "https://twitter.com/yourhandle",
+              "https://linkedin.com/in/yourhandle"
+            ]
+          })
+        }} />
+      </Head>
+      <Component />
+    </>
+  )
 }
